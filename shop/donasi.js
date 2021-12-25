908e0b7081e2edd8e0c199f8eead90e42ac9ab8b
YubiSora <94666378+YubiSora@users.noreply.github.com>
2021-12-25 11:09:14 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,8 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI PULSA
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
+*📲 PULSA*
+ㅁ089674545497
 `
 	}
 
